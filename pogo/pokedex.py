f470710f891640351ae92285fd600d0f1d757002
polzy <polzy@users.noreply.github.com>
2016-08-02 11:12:03 +0200

    Update pokedex.py
    Translate to French

--- pogo/pokedex.py
+++ pogo/pokedex.py
@@ -5,155 +5,155 @@ class Pokedex(dict):
 
     # Enum pokemonIds
     MISSINGNO = 0
-    BULBASAUR = 1
-    IVYSAUR = 2
-    VENUSAUR = 3
-    CHARMANDER = 4
-    CHARMELEON = 5
-    CHARIZARD = 6
-    SQUIRTLE = 7
-    WARTORTLE = 8
-    BLASTOISE = 9
-    CATERPIE = 10
-    METAPOD = 11
-    BUTTERFREE = 12
-    WEEDLE = 13
-    KAKUNA = 14
-    BEEDRILL = 15
-    PIDGEY = 16
-    PIDGEOTTO = 17
-    PIDGEOT = 18
+    BULBIZARRE = 1
+    HERBIZARRE = 2
+    FLORIZARRE = 3
+    SALAMECHE = 4
+    REPTINCEL = 5
+    DRACAUFEU = 6
+    CARAPUCE = 7
+    CARABAFFE = 8
+    TORTANK = 9
+    CHENIPAN = 10
+    CHRYSACIER = 11
+    PAPILUSION = 12
+    ASPICOT = 13
+    COCONFORT = 14
+    DARDARGNAN = 15
+    ROUCOOL = 16
+    ROUCOUPS = 17
+    ROUCARNAGE = 18
     RATTATA = 19
-    RATICATE = 20
-    SPEAROW = 21
-    FEAROW = 22
-    EKANS = 23
+    RATTATAC = 20
+    PIAFABEC = 21
+    RAPASDEPIC = 22
+    ABO = 23
     ARBOK = 24
     PIKACHU = 25
     RAICHU = 26
-    SANDSHREW = 27
-    SANDSLASH = 28
+    SABELETTE = 27
+    SABLAIREAU = 28
     NIDORAN_FEMALE = 29
     NIDORINA = 30
     NIDOQUEEN = 31
     NIDORAN_MALE = 32
     NIDORINO = 33
     NIDOKING = 34
-    CLEFAIRY = 35
-    CLEFABLE = 36
-    VULPIX = 37
-    NINETALES = 38
-    JIGGLYPUFF = 39
-    WIGGLYTUFF = 40
-    ZUBAT = 41
-    GOLBAT = 42
-    ODDISH = 43
-    GLOOM = 44
-    VILEPLUME = 45
+    MELOFEE = 35
+    MELODELFE = 36
+    GOUPIX = 37
+    FEUNARD = 38
+    RONDOUDOU = 39
+    GRODOUDOU = 40
+    NOSFERAPTI = 41
+    NOSFERALTO = 42
+    MYSTHERBE = 43
+    ORTIDE = 44
+    RAFFLESIA = 45
     PARAS = 46
     PARASECT = 47
-    VENONAT = 48
-    VENOMOTH = 49
-    DIGLETT = 50
-    DUGTRIO = 51
-    MEOWTH = 52
+    MIMITOSS = 48
+    AEROMITE = 49
+    TAUPIQUEUR = 50
+    TRIOPIKEUR = 51
+    MIAOUSS = 52
     PERSIAN = 53
-    PSYDUCK = 54
-    GOLDUCK = 55
-    MANKEY = 56
-    PRIMEAPE = 57
-    GROWLITHE = 58
-    ARCANINE = 59
-    POLIWAG = 60
-    POLIWHIRL = 61
-    POLIWRATH = 62
+    PSYKOKWAK = 54
+    AKWAKWAK = 55
+    FEROSINGE = 56
+    COLOSSINGE = 57
+    CANINOS = 58
+    ARCANIN = 59
+    PTITARD = 60
+    TETARTE = 61
+    TARTARD = 62
     ABRA = 63
     KADABRA = 64
     ALAKAZAM = 65
-    MACHOP = 66
-    MACHOKE = 67
-    MACHAMP = 68
-    BELLSPROUT = 69
-    WEEPINBELL = 70
-    VICTREEBEL = 71
+    MACHOC = 66
+    MACHOPEUR = 67
+    MACKOGNEUR = 68
+    CHETIFLOR = 69
+    BOUSTIFLOR = 70
+    EMPIFLOR = 71
     TENTACOOL = 72
     TENTACRUEL = 73
-    GEODUDE = 74
-    GRAVELER = 75
+    RACAILLOU = 74
+    GRAVALANCH = 75
     GOLEM = 76
     PONYTA = 77
-    RAPIDASH = 78
-    SLOWPOKE = 79
-    SLOWBRO = 80
-    MAGNEMITE = 81
+    GALOPA = 78
+    RAMOLOSS = 79
+    FLAGADOSS = 80
+    MAGNETI = 81
     MAGNETON = 82
-    FARFETCHD = 83
+    CANARTICHO = 83
     DODUO = 84
     DODRIO = 85
-    SEEL = 86
-    DEWGONG = 87
-    GRIMER = 88
-    MUK = 89
-    SHELLDER = 90
-    CLOYSTER = 91
-    GASTLY = 92
-    HAUNTER = 93
-    GENGAR = 94
+    OTARIA = 86
+    LAMANTINE = 87
+    TADMORV = 88
+    GROTADMORV = 89
+    KOKIYAS = 90
+    CRUSTABRI = 91
+    FANTOMINUS = 92
+    SPECTRUM = 93
+    ECTOPLASMA = 94
     ONIX = 95
-    DROWZEE = 96
-    HYPNO = 97
+    SOPORIFIK = 96
+    HYPNOMADE = 97
     KRABBY = 98
-    KINGLER = 99
-    VOLTORB = 100
+    KRABBOSS = 99
+    VOLTORBE = 100
     ELECTRODE = 101
-    EXEGGCUTE = 102
-    EXEGGUTOR = 103
-    CUBONE = 104
-    MAROWAK = 105
+    NOEUNOEUF = 102
+    NOADKOKO = 103
+    OSSELAIT = 104
+    OSSATUEUR = 105
     HITMONLEE = 106
-    HITMONCHAN = 107
-    LICKITUNG = 108
-    KOFFING = 109
-    WEEZING = 110
-    RHYHORN = 111
-    RHYDON = 112
-    CHANSEY = 113
-    TANGELA = 114
-    KANGASKHAN = 115
-    HORSEA = 116
-    SEADRA = 117
-    GOLDEEN = 118
-    SEAKING = 119
-    STARYU = 120
-    STARMIE = 121
+    TYGNON = 107
+    EXCELANGUE = 108
+    SMOGO = 109
+    SMOGOGO = 110
+    RHINOCORNE = 111
+    RHINOFEROS = 112
+    LEVEINARD = 113
+    SAQUEDENEU = 114
+    KANGOUREX = 115
+    HYPOTREMPE = 116
+    HYPOCEAN = 117
+    POISSIRENE = 118
+    POISSOROY = 119
+    STARI = 120
+    STAROSS = 121
     MR_MIME = 122
-    SCYTHER = 123
-    JYNX = 124
-    ELECTABUZZ = 125
+    INSECATEUR = 123
+    LIPPOUTOU = 124
+    ELEKTEK = 125
     MAGMAR = 126
-    PINSIR = 127
+    SCARABRUTE = 127
     TAUROS = 128
-    MAGIKARP = 129
-    GYARADOS = 130
-    LAPRAS = 131
-    DITTO = 132
-    EEVEE = 133
-    VAPOREON = 134
-    JOLTEON = 135
-    FLAREON = 136
+    MAGICARPE = 129
+    LEVIATOR = 130
+    LOKHLASS = 131
+    METAMORPH = 132
+    EVOLI = 133
+    AQUALI = 134
+    VOLTALI = 135
+    PYROLI = 136
     PORYGON = 137
-    OMANYTE = 138
-    OMASTAR = 139
+    AMONITA = 138
+    AMONISTAR = 139
     KABUTO = 140
     KABUTOPS = 141
-    AERODACTYL = 142
-    SNORLAX = 143
-    ARTICUNO = 144
-    ZAPDOS = 145
-    MOLTRES = 146
-    DRATINI = 147
-    DRAGONAIR = 148
-    DRAGONITE = 149
+    PTERA = 142
+    RONFLEX = 143
+    ARTIKODIN = 144
+    ELECTHOR = 145
+    SULFURA = 146
+    MINIDRACO = 147
+    DRACO = 148
+    DRACOLOSSE = 149
     MEWTWO = 150
     MEW = 151
 
@@ -172,81 +172,81 @@ class Pokedex(dict):
         # Ideally go back and lint for line lengths
         self.rarity[Rarity.MYTHIC] = [self.MEW]
         self.rarity[Rarity.LEGENDARY] = [
-            self.ZAPDOS, self.MOLTRES, self.MEWTWO, self.ARTICUNO
+            self.ELECTHOR, self.SULFURA, self.MEWTWO, self.ARTIKODIN
         ]
         self.rarity[Rarity.EPIC] = [
-            self.DITTO, self.VENUSAUR, self.TAUROS, self.DRAGONITE, self.CLEFABLE,
-            self.CHARIZARD, self.BLASTOISE
+            self.METAMORPH, self.FLORIZARRE, self.TAUROS, self.DRACOLOSSE, self.MELODELFE,
+            self.DRACAUFEU, self.TORTANK
         ]
         self.rarity[Rarity.VERY_RARE] = [
-            self.WEEPINBELL, self.WARTORTLE, self.VILEPLUME, self.VICTREEBEL,
-            self.VENOMOTH, self.VAPOREON, self.SLOWBRO, self.RAICHU, self.POLIWRATH,
-            self.PINSIR, self.PIDGEOT, self.OMASTAR, self.NIDOQUEEN, self.NIDOKING,
-            self.MUK, self.MAROWAK, self.LAPRAS, self.KANGASKHAN, self.KABUTOPS, self.IVYSAUR,
-            self.GYARADOS, self.GOLEM, self.GENGAR, self.EXEGGUTOR, self.DRAGONAIR, self.DEWGONG,
-            self.CHARMELEON, self.BEEDRILL, self.ALAKAZAM
+            self.BOUSTIFLOR, self.CARABAFFE, self.RAFFLESIA, self.EMPIFLOR,
+            self.AEROMITE, self.AQUALI, self.FLAGADOSS, self.RAICHU, self.TARTARD,
+            self.SCARABRUTE, self.ROUCARNAGE, self.AMONISTAR, self.NIDOQUEEN, self.NIDOKING,
+            self.GROTADMORV, self.OSSATUEUR, self.LOKHLASS, self.KANGOUREX, self.KABUTOPS, self.HERBIZARRE,
+            self.LEVIATOR, self.GOLEM, self.ECTOPLASMA, self.NOADKOKO, self.DRACO, self.LAMANTINE,
+            self.REPTINCEL, self.DARDARGNAN, self.ALAKAZAM
         ]
         self.rarity[Rarity.RARE] = [
-            self.WIGGLYTUFF, self.WEEZING, self.TENTACRUEL, self.TANGELA,
-            self.STARMIE, self.SNORLAX, self.SCYTHER, self.SEAKING, self.SEADRA,
-            self.RHYDON, self.RAPIDASH, self.PRIMEAPE, self.PORYGON, self.POLIWHIRL,
-            self.PARASECT, self.ONIX, self.OMANYTE, self.NINETALES, self.NIDORINO,
-            self.NIDORINA, self.MR_MIME, self.MAGMAR, self.MACHOKE, self.MACHAMP,
-            self.LICKITUNG, self.KINGLER, self.JOLTEON, self.HYPNO, self.HITMONCHAN,
-            self.GLOOM, self.GOLDUCK, self.FLAREON, self.FEAROW, self.FARFETCHD,
-            self.ELECTABUZZ, self.DUGTRIO, self.DRATINI, self.DODRIO, self.CLOYSTER,
-            self.CHANSEY, self.BUTTERFREE, self.ARCANINE, self.AERODACTYL
+            self.GRODOUDOU, self.SMOGOGO, self.TENTACRUEL, self.SAQUEDENEU,
+            self.STAROSS, self.RONFLEX, self.INSECATEUR, self.POISSOROY, self.HYPOCEAN,
+            self.RHINOFEROS, self.GALOPA, self.COLOSSINGE, self.PORYGON, self.TETARTE,
+            self.PARASECT, self.ONIX, self.AMONITA, self.FEUNARD, self.NIDORINO,
+            self.NIDORINA, self.MR_MIME, self.MAGMAR, self.MACHOPEUR, self.MACKOGNEUR,
+            self.EXCELANGUE, self.KRABBOSS, self.VOLTALI, self.HYPNOMADE, self.TYGNON,
+            self.ORTIDE, self.AKWAKWAK, self.PYROLI, self.RAPASDEPIC, self.CANARTICHO,
+            self.ELEKTEK, self.TRIOPIKEUR, self.MINIDRACO, self.DODRIO, self.CRUSTABRI,
+            self.LEVEINARD, self.PAPILUSION, self.ARCANIN, self.PTERA
         ]
         self.rarity[Rarity.UNCOMMON] = [
-            self.VULPIX, self.TENTACOOL, self.STARYU, self.SQUIRTLE, self.SPEAROW,
-            self.SHELLDER, self.SEEL, self.SANDSLASH, self.RHYHORN, self.RATICATE,
-            self.PSYDUCK, self.PONYTA, self.PIKACHU, self.PIDGEOTTO, self.PERSIAN,
-            self.METAPOD, self.MAGNETON, self.KOFFING, self.KADABRA, self.KABUTO,
-            self.KAKUNA, self.JYNX, self.JIGGLYPUFF, self.HORSEA, self.HITMONLEE,
-            self.HAUNTER, self.GROWLITHE, self.GRIMER, self.GRAVELER, self.GOLBAT,
-            self.EXEGGCUTE, self.ELECTRODE, self.CUBONE, self.CLEFAIRY, self.CHARMANDER,
-            self.BULBASAUR, self.ARBOK, self.ABRA
+            self.GOUPIX, self.TENTACOOL, self.STARI, self.CARAPUCE, self.PIAFABEC,
+            self.KOKIYAS, self.OTARIA, self.SABLAIREAU, self.RHINOCORNE, self.RATTATAC,
+            self.PSYKOKWAK, self.PONYTA, self.PIKACHU, self.ROUCOUPS, self.PERSIAN,
+            self.CHRYSACIER, self.MAGNETON, self.SMOGO, self.KADABRA, self.KABUTO,
+            self.COCONFORT, self.LIPPOUTOU, self.RONDOUDOU, self.HYPOTREMPE, self.HITMONLEE,
+            self.SPECTRUM, self.CANINOS, self.TADMORV, self.GRAVALANCH, self.NOSFERALTO,
+            self.NOEUNOEUF, self.ELECTRODE, self.OSSELAIT, self.MELOFEE, self.SALAMECHE,
+            self.BULBIZARRE, self.ARBOK, self.ABRA
         ]
         self.rarity[Rarity.COMMON] = [
-            self.WEEDLE, self.VOLTORB, self.VENONAT, self.SLOWPOKE, self.SANDSHREW,
-            self.POLIWAG, self.PARAS, self.ODDISH, self.NIDORAN_MALE, self.NIDORAN_FEMALE,
-            self.MEOWTH, self.MANKEY, self.MAGNEMITE, self.MAGIKARP, self.MACHOP, self.KRABBY,
-            self.GOLDEEN, self.GEODUDE, self.GASTLY, self.EEVEE, self.EKANS, self.DROWZEE,
-            self.DODUO, self.DIGLETT, self.CATERPIE, self.BELLSPROUT
+            self.ASPICOT, self.VOLTORBE, self.MIMITOSS, self.RAMOLOSS, self.SABELETTE,
+            self.PTITARD, self.PARAS, self.MYSTHERBE, self.NIDORAN_MALE, self.NIDORAN_FEMALE,
+            self.MIAOUSS, self.FEROSINGE, self.MAGNETI, self.MAGICARPE, self.MACHOC, self.KRABBY,
+            self.POISSIRENE, self.RACAILLOU, self.FANTOMINUS, self.EVOLI, self.ABO, self.SOPORIFIK,
+            self.DODUO, self.TAUPIQUEUR, self.CHENIPAN, self.CHETIFLOR
         ]
-        self.rarity[Rarity.CRITTER] = [self.ZUBAT, self.PIDGEY, self.RATTATA]
+        self.rarity[Rarity.CRITTER] = [self.NOSFERAPTI, self.ROUCOOL, self.RATTATA]
 
         self.evolves = {
-            self.MISSINGNO: 0, self.BULBASAUR: 25, self.IVYSAUR: 100, self.VENUSAUR: 0,
-            self.CHARMANDER: 25, self.CHARMELEON: 100, self.CHARIZARD: 0, self.SQUIRTLE: 25,
-            self.WARTORTLE: 100, self.BLASTOISE: 0, self.CATERPIE: 12, self.METAPOD: 50,
-            self.BUTTERFREE: 0, self.WEEDLE: 12, self.KAKUNA: 50, self.BEEDRILL: 0, self.PIDGEY: 12,
-            self.PIDGEOTTO: 50, self.PIDGEOT: 0, self.RATTATA: 25, self.RATICATE: 0, self.SPEAROW: 50,
-            self.FEAROW: 0, self.EKANS: 50, self.ARBOK: 0, self.PIKACHU: 50, self.RAICHU: 0,
-            self.SANDSHREW: 50, self.SANDSLASH: 0, self.NIDORAN_FEMALE: 25, self.NIDORINA: 100,
+            self.MISSINGNO: 0, self.BULBIZARRE: 25, self.HERBIZARRE: 100, self.FLORIZARRE: 0,
+            self.SALAMECHE: 25, self.REPTINCEL: 100, self.DRACAUFEU: 0, self.CARAPUCE: 25,
+            self.CARABAFFE: 100, self.TORTANK: 0, self.CHENIPAN: 12, self.CHRYSACIER: 50,
+            self.PAPILUSION: 0, self.ASPICOT: 12, self.COCONFORT: 50, self.DARDARGNAN: 0, self.ROUCOOL: 12,
+            self.ROUCOUPS: 50, self.ROUCARNAGE: 0, self.RATTATA: 25, self.RATTATAC: 0, self.PIAFABEC: 50,
+            self.RAPASDEPIC: 0, self.ABO: 50, self.ARBOK: 0, self.PIKACHU: 50, self.RAICHU: 0,
+            self.SABELETTE: 50, self.SABLAIREAU: 0, self.NIDORAN_FEMALE: 25, self.NIDORINA: 100,
             self.NIDOQUEEN: 0, self.NIDORAN_MALE: 25, self.NIDORINO: 100, self.NIDOKING: 0,
-            self.CLEFAIRY: 50, self.CLEFABLE: 0, self.VULPIX: 50, self.NINETALES: 0, self.JIGGLYPUFF: 50,
-            self.WIGGLYTUFF: 0, self.ZUBAT: 50, self.GOLBAT: 0, self.ODDISH: 25, self.GLOOM: 100,
-            self.VILEPLUME: 0, self.PARAS: 50, self.PARASECT: 0, self.VENONAT: 50, self.VENOMOTH: 0,
-            self.DIGLETT: 50, self.DUGTRIO: 0, self.MEOWTH: 50, self.PERSIAN: 0, self.PSYDUCK: 50,
-            self.GOLDUCK: 0, self.MANKEY: 50, self.PRIMEAPE: 0, self.GROWLITHE: 50, self.ARCANINE: 0,
-            self.POLIWAG: 25, self.POLIWHIRL: 100, self.POLIWRATH: 0, self.ABRA: 25, self.KADABRA: 100,
-            self.ALAKAZAM: 0, self.MACHOP: 25, self.MACHOKE: 100, self.MACHAMP: 0, self.BELLSPROUT: 25,
-            self.WEEPINBELL: 100, self.VICTREEBEL: 0, self.TENTACOOL: 50, self.TENTACRUEL: 0,
-            self.GEODUDE: 25, self.GRAVELER: 100, self.GOLEM: 0, self.PONYTA: 50, self.RAPIDASH: 0,
-            self.SLOWPOKE: 50, self.SLOWBRO: 0, self.MAGNEMITE: 50, self.MAGNETON: 0, self.FARFETCHD: 0,
-            self.DODUO: 50, self.DODRIO: 0, self.SEEL: 50, self.DEWGONG: 0, self.GRIMER: 50, self.MUK: 0,
-            self.SHELLDER: 50, self.CLOYSTER: 0, self.GASTLY: 25, self.HAUNTER: 100, self.GENGAR: 0,
-            self.ONIX: 0, self.DROWZEE: 50, self.HYPNO: 0, self.KRABBY: 50, self.KINGLER: 0, self.VOLTORB: 50,
-            self.ELECTRODE: 0, self.EXEGGCUTE: 50, self.EXEGGUTOR: 0, self.CUBONE: 50, self.MAROWAK: 0,
-            self.HITMONLEE: 0, self.HITMONCHAN: 0, self.LICKITUNG: 0, self.KOFFING: 50, self.WEEZING: 0,
-            self.RHYHORN: 50, self.RHYDON: 0, self.CHANSEY: 0, self.TANGELA: 0, self.KANGASKHAN: 0,
-            self.HORSEA: 50, self.SEADRA: 0, self.GOLDEEN: 50, self.SEAKING: 0, self.STARYU: 50, self.STARMIE: 0,
-            self.MR_MIME: 0, self.SCYTHER: 0, self.JYNX: 0, self.ELECTABUZZ: 0, self.MAGMAR: 0, self.PINSIR: 0,
-            self.TAUROS: 0, self.MAGIKARP: 400, self.GYARADOS: 0, self.LAPRAS: 0, self.DITTO: 0, self.EEVEE: 25,
-            self.VAPOREON: 0, self.JOLTEON: 0, self.FLAREON: 0, self.PORYGON: 0, self.OMANYTE: 50, self.OMASTAR: 0,
-            self.KABUTO: 50, self.KABUTOPS: 0, self.AERODACTYL: 0, self.SNORLAX: 0, self.ARTICUNO: 0,
-            self.ZAPDOS: 0, self.MOLTRES: 0, self.DRATINI: 25, self.DRAGONAIR: 100, self.DRAGONITE: 0,
+            self.MELOFEE: 50, self.MELODELFE: 0, self.GOUPIX: 50, self.FEUNARD: 0, self.RONDOUDOU: 50,
+            self.GRODOUDOU: 0, self.NOSFERAPTI: 50, self.NOSFERALTO: 0, self.MYSTHERBE: 25, self.ORTIDE: 100,
+            self.RAFFLESIA: 0, self.PARAS: 50, self.PARASECT: 0, self.MIMITOSS: 50, self.AEROMITE: 0,
+            self.TAUPIQUEUR: 50, self.TRIOPIKEUR: 0, self.MIAOUSS: 50, self.PERSIAN: 0, self.PSYKOKWAK: 50,
+            self.AKWAKWAK: 0, self.FEROSINGE: 50, self.COLOSSINGE: 0, self.CANINOS: 50, self.ARCANIN: 0,
+            self.PTITARD: 25, self.TETARTE: 100, self.TARTARD: 0, self.ABRA: 25, self.KADABRA: 100,
+            self.ALAKAZAM: 0, self.MACHOC: 25, self.MACHOPEUR: 100, self.MACKOGNEUR: 0, self.CHETIFLOR: 25,
+            self.BOUSTIFLOR: 100, self.EMPIFLOR: 0, self.TENTACOOL: 50, self.TENTACRUEL: 0,
+            self.RACAILLOU: 25, self.GRAVALANCH: 100, self.GOLEM: 0, self.PONYTA: 50, self.GALOPA: 0,
+            self.RAMOLOSS: 50, self.FLAGADOSS: 0, self.MAGNETI: 50, self.MAGNETON: 0, self.CANARTICHO: 0,
+            self.DODUO: 50, self.DODRIO: 0, self.OTARIA: 50, self.LAMANTINE: 0, self.TADMORV: 50, self.GROTADMORV: 0,
+            self.KOKIYAS: 50, self.CRUSTABRI: 0, self.FANTOMINUS: 25, self.SPECTRUM: 100, self.ECTOPLASMA: 0,
+            self.ONIX: 0, self.SOPORIFIK: 50, self.HYPNOMADE: 0, self.KRABBY: 50, self.KRABBOSS: 0, self.VOLTORBE: 50,
+            self.ELECTRODE: 0, self.NOEUNOEUF: 50, self.NOADKOKO: 0, self.OSSELAIT: 50, self.OSSATUEUR: 0,
+            self.HITMONLEE: 0, self.TYGNON: 0, self.EXCELANGUE: 0, self.SMOGO: 50, self.SMOGOGO: 0,
+            self.RHINOCORNE: 50, self.RHINOFEROS: 0, self.LEVEINARD: 0, self.SAQUEDENEU: 0, self.KANGOUREX: 0,
+            self.HYPOTREMPE: 50, self.HYPOCEAN: 0, self.POISSIRENE: 50, self.POISSOROY: 0, self.STARI: 50, self.STAROSS: 0,
+            self.MR_MIME: 0, self.INSECATEUR: 0, self.LIPPOUTOU: 0, self.ELEKTEK: 0, self.MAGMAR: 0, self.SCARABRUTE: 0,
+            self.TAUROS: 0, self.MAGICARPE: 400, self.LEVIATOR: 0, self.LOKHLASS: 0, self.METAMORPH: 0, self.EVOLI: 25,
+            self.AQUALI: 0, self.VOLTALI: 0, self.PYROLI: 0, self.PORYGON: 0, self.AMONITA: 50, self.AMONISTAR: 0,
+            self.KABUTO: 50, self.KABUTOPS: 0, self.PTERA: 0, self.RONFLEX: 0, self.ARTIKODIN: 0,
+            self.ELECTHOR: 0, self.SULFURA: 0, self.MINIDRACO: 25, self.DRACO: 100, self.DRACOLOSSE: 0,
             self.MEWTWO: 0, self.MEW: 0
         }
 
