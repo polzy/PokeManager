5e15c4a1b5cc5d5251bd5116c549a9ad62d5cdce
polzy <polzy@users.noreply.github.com>
2016-08-09 12:52:32 +0200

    Change rename to Pokemon IV%
    Change rename to Pokemon IV%

--- pogo/demo.py
+++ pogo/demo.py
@@ -250,7 +250,7 @@ def massRename(session):
 		logging.info('\nNo Pokemon to be renamed')
 		mainMenu(session)
 	
-	logging.info('\nThis will rename %s Pokemon.',len(refinedParty))
+	logging.info('\nThis will rename %s Pokemon .',len(refinedParty))
 	okayToProceed = raw_input('Do you want to rename these Pokemon? (y/n): ').lower()
 	
 	# Rename the pokemon! Use randomness to reduce chance of bot detection
